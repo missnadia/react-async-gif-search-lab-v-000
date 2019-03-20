f5498e666f0e48e396377bfd5e21b7693dc00bea
Nadia Lee <miss.nadia.lee@gmail.com>
2019-03-20 20:45:03 +0000

    Automatically backed up by Learn

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -9,12 +9,19 @@ export default class GifSearch extends Component {
     event.preventDefault()
     this.props.fetchGIFs(this.state.query)
   }
+  
+  handleChange = event => { 
+    event.persist() 
+    this.setState({ 
+      [event.target.id]: event.target.value 
+    }) 
+  }
 
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.query} onChange={event => this.setState({query: event.target.value})} />
+          <input type="text" value={this.state.query} onChange={this.handleChange} />
         </form>
       </div>
     )
