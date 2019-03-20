821fd12ccecb07e003b8e7f75f5b2c97d7967259
Nadia Lee <miss.nadia.lee@gmail.com>
2019-03-20 20:43:48 +0000

    Automatically backed up by Learn

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react'
 
-export default class GifList extends Component {
-  
+const GifList = props => {
+  console.log(props);
+  return (
+    <div>
+        {props.gifs.map(gif => <img key={gif.url} src={gif.url} alt="gif"/>)}
+    </div>
+  )
 }
+
+export default GifList
